9ad49e34c96a3022e390d021fccdf74556c19301
Fojan Babaali <fozhanmail@gmail.com>
2021-10-27 17:31:28 -0700

    update

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,25 +8,26 @@ const Header = () => {
       <div className="inline-block absolute top-8 left-36 -mt-2">
         <img src={circle} alt="" className="w-16 h-16" />
       </div>
-      <div className="flex-row absolute right-0 w-2/4 justify-center sm:right-40 w-1/4">
-        <button className="inline-block m-8 text-gray-300 font-bold text-base	bg-primary px-4 py-1">
+      <div className="flex-row absolute right-0 w-2/4 justify-center xl:right-40 w-96">
+        <button className="inline-block m-8 text-gray-300 font-bold text-base	bg-primary px-4 py-1 xl:m-4">
           Submit
         </button>
-        <div className="inline-block m-8 text-gray-300 font-bold text-base">
+        <div className="inline-block m-8 text-gray-300 font-bold text-base xl:m-4">
           Gallery
         </div>
-        <div className="inline-block m-8 text-gray-300 font-bold text-base">
+        <div className="inline-block m-8 text-gray-300 font-bold text-base xl:m-4">
           About
         </div>
-        <div className="inline-block m-8 text-gray-300 font-bold text-base">
+        <div className="inline-block m-8 text-gray-300 font-bold text-base xl:m-4">
           Contact
         </div>
-        <div className="inline-block px-7 text-gray-300 font-bold text-base	mt-10">
-          <FaSearch />
+        <div className="inline-block w-20 justify-evenly text-gray-300 font-bold text-base mt-10 xl:mt-4">
+          <FaSearch className="inline-block ml-4"/>
+          <FaBars className="inline-block ml-8"/>
+
         </div>
-        <div className="inline-block text-gray-300 font-bold text-base px-7">
-          <FaBars />
-        </div>
+        {/* <div className="block text-gray-300 font-bold text-base px-7 xl:mt-4 px-4">
+        </div> */}
       </div>
     </>
   );
