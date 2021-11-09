3eaedd3d0fabef87ee284f15ee8a0f544a8a4eaf
Fojan Babaali <fozhanmail@gmail.com>
2021-11-08 21:51:58 -0800

    Update About.js

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,20 +4,22 @@ const About = () => {
   return (
     <div>
       <hr className="w-5/6 m-auto opacity-40" />
-      <div className="grid grid-rows-1 grid-cols-2 text-gray-100 w-5/6 m-auto py-8">
-        <p className="bg-blob2 bg-contain bg-center bg-no-repeat flex justify-center items-center text-center text-gray-100 font-bold text-3xl">
-          About Sarah
+      <div className="grid grid-rows-1 grid-cols-2 text-gray-100 w-5/6 m-auto py-8 sm:grid-cols-1 sm:gap-8 md:grid-cols-1 md:gap-8 lg:gap-12">
+        <p className="bg-blob2 bg-contain bg-center bg-no-repeat flex justify-center items-center text-center text-gray-100 font-bold text-3xl sm:bg-red-500 sm:bg-opacity-20 sm:rounded-2xl sm:p-2 sm:text-sm sm:bg-none md:bg-red-500 md:bg-opacity-20 md:rounded-2xl md:p-2 md:text-xl md:bg-none">
+          About Fojan
         </p>
 
         <p className="flex justify-center items-center text-left">
-          Sarah is a premier professional photographer located in Vancouver BC
+          Fojan is a premier professional photographer located in Vancouver BC
           Canada and abroad. Her technical skills matched with vibrant energy
           and creative intuition enable him to create stunning visual images.
           Tom Hawkins strives to reveal beauty and vitality though the devices
-          of technical dedication, geometric symmetry and creative exuberance.<br/><br/>
+          of technical dedication, geometric symmetry and creative exuberance.
+          <br />
+          <br />
           The main goal of her work is produce images that reflect the majesty
           and wonder of the human experience. Though her attention to technical
-          detail and inexhaustible energy while shooting, Sarah Nilson delivers
+          detail and inexhaustible energy while shooting, Fojan Babaali delivers
           fresh and modern images that enable her clients to sell products.
         </p>
       </div>
@@ -26,3 +28,4 @@ const About = () => {
 };
 
 export default About;
+
