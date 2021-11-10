8db6ccc54e71800a1bb3095e54e4c5fa8b815396
Fojan Babaali <fozhanmail@gmail.com>
2021-11-09 21:31:46 -0800

    Contact Updated

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -6,11 +6,11 @@ const Contact = () => {
       <hr className="w-5/6 m-auto opacity-40" />
       <div className="grid grid-rows-1 grid-cols-2 text-gray-100 w-5/6 m-auto py-8">
         <p className="flex items-center justify-center text-gray-100 font-bold text-3xl">
-          Contact Sarah
+          Contact Fojan
         </p>
         <p className="flex justify-center items-center text-left">
           For questions relating/regarding pre-sale, cooperation and general
-          issues please reach out info@sarahnilson.ca
+          issues please reach out info@fojanbabaali.ca
         </p>
       </div>
     </div>
