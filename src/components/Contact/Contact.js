dc9f240ce9d4805b0fc99928cea9b35f65bf759a
Fojan Babaali <fozhanmail@gmail.com>
2021-11-09 21:39:24 -0800

    Contact Updated

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -4,8 +4,8 @@ const Contact = () => {
   return (
     <div>
       <hr className="w-5/6 m-auto opacity-40" />
-      <div className="grid grid-rows-1 grid-cols-2 text-gray-100 w-5/6 m-auto py-8">
-        <p className="flex items-center justify-center text-gray-100 font-bold text-3xl">
+      <div className="grid grid-rows-1 grid-cols-2 text-gray-100 w-5/6 m-auto py-8 sm:grid-cols-1 sm:gap-8 md:grid-cols-1 md:gap-8 lg:gap-12">
+        <p className="flex items-center justify-center text-gray-100 font-bold text-3xl sm:bg-red-500 sm:bg-opacity-20 sm:rounded-2xl sm:p-2 sm:text-sm sm:bg-none md:bg-red-500 md:bg-opacity-20 md:rounded-2xl md:p-2 md:text-xl">
           Contact Fojan
         </p>
         <p className="flex justify-center items-center text-left">
