aa41b5d5b49d592d23e3f95fdb2bd825a23bd282
Fojan Babaali <fozhanmail@gmail.com>
2021-11-12 22:19:25 -0800

    Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,14 +1,15 @@
 import React from "react";
 const Footer = () => {
   return (
-    <div className="grid grid-rows-1 grid-cols-5 w-6/12 justify-center items-center m-auto text-gray-300 overflow-hidden bg-red-500 bg-opacity-20 text-sm absolute bottom-10 left-96 md:pb-12 sm:grid-rows-5 sm:grid-cols-1 sm:bg-red-500 sm:bg-opacity-20 sm:pb-10 sm:pt-8 sm:rounded-t-2xl sm:gap-1 sm:text-base">
-      <div className="flex justify-center items-center">Terms & Conditions</div>
-      <div className="flex justify-center items-center">How to Submit?</div>
-      <div className="flex justify-center items-center">FAQ</div>
-      <div className="flex justify-center items-center">Press</div>
-      <div className="flex justify-center items-center text-center">
-        &copy;2021-Fojan Babaali
-      </div>
+    <div className="bg-red-500 bg-opacity-20">
+        <div className="grid grid-rows-1 grid-cols-4 justify-center items-center text-center w-1/2 mt-24 p-10 m-auto text-gray-300 overflow-hidden text-sm lg:w-1/2 md:text-sm md:grid-cols-2 md:w-1/2 sm:grid-rows-5 sm:grid-cols-1 sm:pb-10 sm:pt-8 sm:gap-1 sm:text-base">
+          <div className="flex flex-row justify-center items-center">Terms & Conditions</div>
+          <div className="flex flex-row justify-center items-center">How to Submit?</div>
+          <div className="flex flex-row justify-center items-center">FAQ</div>
+          <div className="flex flex-row justify-center items-center text-center">
+            &copy;2021-Fojan Babaali
+          </div>
+        </div>
     </div>
   );
 };
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,21 +1,38 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <div id="navbar" className="grid grid-rows-1 grid-cols-4 justify-center text-lg items-center bg-red-500 bg-opacity-20 rounded-b-2xl py-8 w-1/2 text-gray-100 m-auto lg:w-3/4 md:w-3/4 sm:grid-cols-1 sm:gap-4 sm:bg-red-500 sm:bg-opacity-20 sm:pb-10 sm:rounded-b-2xl">
-      <Link to="/" className="flex justify-center items-center font-bold text-lg transform hover:scale-110 hover:transition duration-500 ease-in-out sm:text-l">
-        Home
-      </Link>
-      <Link to="/about" className="flex justify-center items-center font-bold text-lg transform hover:scale-110 hover:transition duration-500 ease-in-out sm:text-l">
-        About
-      </Link>
-      <Link to="/gallery" className="flex justify-center items-center font-bold text-lg transform hover:scale-110 hover:transition duration-500 ease-in-out sm:text-l">
-        Gallery
-      </Link>
-      <Link to="/contact" className="flex justify-center items-center font-bold text-lg transform hover:scale-110 hover:transition duration-500 ease-in-out sm:text-l">
-        Contact
-      </Link>
+    <div className="bg-red-500 bg-opacity-20">
+      <div
+        id="navbar"
+        className="grid grid-rows-1 grid-cols-4 justify-center text-lg items-center  py-8 w-1/2 text-gray-100 m-auto lg:w-3/4 md:w-3/4 sm:grid-cols-1 sm:gap-4 sm:pb-10"
+      >
+        <Link
+          to="/"
+          className="flex justify-center items-center font-bold text-lg transform hover:scale-110 hover:transition duration-500 ease-in-out sm:text-l"
+        >
+          Home
+        </Link>
+        <Link
+          to="/about"
+          className="flex justify-center items-center font-bold text-lg transform hover:scale-110 hover:transition duration-500 ease-in-out sm:text-l"
+        >
+          About
+        </Link>
+        <Link
+          to="/gallery"
+          className="flex justify-center items-center font-bold text-lg transform hover:scale-110 hover:transition duration-500 ease-in-out sm:text-l"
+        >
+          Gallery
+        </Link>
+        <Link
+          to="/contact"
+          className="flex justify-center items-center font-bold text-lg transform hover:scale-110 hover:transition duration-500 ease-in-out sm:text-l"
+        >
+          Contact
+        </Link>
+      </div>
     </div>
   );
 };
