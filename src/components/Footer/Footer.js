6678e19f9a9e04cb10d3e300a04c25721640bae0
Fojan Babaali <fozhanmail@gmail.com>
2021-11-14 19:53:46 -0800

    Form Updated

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,11 +2,11 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="grid grid-cols-4 justify-center text-center items-center m-0 m-auto max-h-screen absolute bottom-0 py-10 w-full text-gray-300 bg-grey bg-opacity-40">
-      <div>Socials</div>
-      <div>Services</div>
-      <div>FAQ</div>
-      <div>Contact</div>
+    <footer className="grid grid-cols-4 justify-center text-center items-center m-0 m-auto max-h-screen absolute bottom-0 py-10 w-full text-gray-300 bg-grey bg-opacity-40 sm:grid-cols-1 sm:py-4 sm:text-md sm:mt-8 ">
+      <div className="flex justify-center items-center text-center">Socials</div>
+      <div className="flex justify-center items-center text-center">Services</div>
+      <div className="flex justify-center items-center text-center">FAQ</div>
+      <div className="flex justify-center items-center text-center">Contact</div>
     </footer>
   );
 };
--- src/components/Join/Join.js
+++ src/components/Join/Join.js
@@ -2,13 +2,13 @@ import React from 'react'
 
 const Join = () => {
     return (
-        <form action="" className="grid grid-cols-1 m-auto w-80 m-40 gap-8 bg-secondary rounded bg-opacity-20 p-8">
+        <form action="" className="grid grid-cols-1 m-auto w-auto mt-10 gap-6 rounded p-8 lg:mt-8 sm:gap-4 md:gap-4 sm:mt-10 sm:mb-10">
             <input type="text" placeholder="First Name" name="fname" id="fname" className="h-10 p-4 border rounded m-auto"/>
             <input type="text" placeholder="Last Name" name="lname" id="fname" className="h-10 p-4 border rounded m-auto"/>
             <input type="email" placeholder="Email" name="email" id="email" className="h-10 p-4 border rounded m-auto"/>
             <input type="number" placeholder="Phone Number" name="phone" id="phone" className="h-10 p-4 border rounded m-auto"/>
             <input type="text" placeholder="Postal Code" name="pcode" id="pcode" className="h-10 p-4 border rounded m-auto"/>
-            <button type="submit" className="bg-secondary font-bold border rounded m-auto text-lg px-8 py-2 text-gray-100 transform hover:scale-110 hover:transition duration-500 ease-in-out sm:text-sm sm:w-40 sm:h-10 sm:mb-5">Submit</button>
+            <button type="submit" className="bg-secondary font-bold border rounded m-auto text-lg px-8 py-2 text-gray-100 transform hover:scale-110 hover:transition duration-500 ease-in-out sm:px-4 sm:text-sm sm:w-20 sm:h-10 sm:mb-5">Submit</button>
         </form>
     )
 }
