ab96d68abfa0f26d4918fde52c4aa221f3bc14c5
Fojan Babaali <fozhanmail@gmail.com>
2021-11-16 12:21:36 -0800

    UI Updated

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -5,7 +5,7 @@ const About = () => {
     <div>
       {/* <hr className="w-5/6 m-auto opacity-40" /> */}
       <div className="grid grid-rows-1 grid-cols-2 text-gray-100 w-8/12 m-auto py-24 sm:grid-cols-1 sm:gap-4 md:grid-cols-1 md:gap-4 lg:gap-8 lg:grid-cols-1 lg:w-3/6">
-        <p className="bg-blob2 bg-contain bg-center bg-no-repeat flex justify-center items-center text-center text-gray-100 font-bold text-3xl z-20 sm:bg-secondary sm:rounded-2xl sm:p-2 sm:text-sm sm:bg-none md:bg-secondary md:rounded-2xl md:p-2 md:text-xl md:bg-none">
+        <p className="bg-blob4 bg-contain bg-center bg-no-repeat flex justify-center items-center text-center text-gray-100 font-bold text-3xl p-20 z-20 sm:bg-secondary sm:rounded-2xl sm:p-2 sm:text-sm sm:bg-none md:bg-secondary md:rounded-2xl md:p-2 md:text-xl md:bg-none">
           About Fojan
         </p>
 
