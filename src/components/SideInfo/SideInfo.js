b16859e189a070f57ad1ee7dd5cbbb9f5273a32c
Fojan Babaali <fozhanmail@gmail.com>
2021-11-17 16:17:21 -0800

    Update

--- src/components/SideInfo/SideInfo.js
+++ src/components/SideInfo/SideInfo.js
@@ -12,20 +12,34 @@ const SideInfo = () => {
   return (
     <div className="">
       <div className="bg-gradient-to-b from-indigo-900 via-indigo-900 to-gray-900 grid grid-cols-1 absolute left-0 top-0 text-gray-300 w-20 min-h-full justify-center items-center text-center sm:hidden md:hidden">
-        <div className="flex flex-col justify-between items-center text-center absolute top-10 left-8 m-auto transform hover:scale-125 hover:text-gray-100 hover:transition duration-500 ease-in-out">
-          <FaBars />
-        </div>
-        <div className="flex flex-col justify-between items-center text-center h-60 m-0 m-auto">
-          <Link to="/google-login" className="transform hover:scale-125 hover:text-gray-100 hover:transition duration-500 ease-in-out">
+        <div className="flex flex-col justify-between items-center text-center h-60 m-0 m-auto z-50 fixed top-32 left-8">
+          <div className="transform hover:scale-125 hover:text-gray-100 hover:transition duration-500 ease-in-out">
+            {" "}
+            <FaBars />
+          </div>
+
+          <Link
+            to="/google-login"
+            className="transform hover:scale-125 hover:text-gray-100 hover:transition duration-500 ease-in-out"
+          >
             <FaEnvelope />
           </Link>
-          <Link to="/fojan-twitter" className="transform hover:scale-125 hover:text-gray-100 hover:transition duration-500 ease-in-out">
+          <Link
+            to="/fojan-twitter"
+            className="transform hover:scale-125 hover:text-gray-100 hover:transition duration-500 ease-in-out"
+          >
             <FaTwitter />
           </Link>
-          <Link to="/fojan-instagram" className="transform hover:scale-125 hover:text-gray-100 hover:transition duration-500 ease-in-out">
+          <Link
+            to="/fojan-instagram"
+            className="transform hover:scale-125 hover:text-gray-100 hover:transition duration-500 ease-in-out"
+          >
             <FaInstagram />
           </Link>
-          <Link to="/fojan-linkedin" className="transform hover:scale-125 hover:text-gray-100 hover:transition duration-500 ease-in-out">
+          <Link
+            to="/fojan-linkedin"
+            className="transform hover:scale-125 hover:text-gray-100 hover:transition duration-500 ease-in-out"
+          >
             <FaLinkedinIn />
           </Link>
         </div>
