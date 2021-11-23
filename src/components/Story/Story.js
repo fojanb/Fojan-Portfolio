941d86bb6138f23d39e28cca94628e27f89c116e
Fojan Babaali <fozhanmail@gmail.com>
2021-11-22 22:57:06 -0800

    Story-responsive done

--- src/components/Story/Story.js
+++ src/components/Story/Story.js
@@ -7,8 +7,8 @@ const Story = () => {
       <span className="bg-blob4 bg-contain bg-center bg-no-repeat flex justify-center text-2xl font-bold items-center p-14 text-gray-300 text-shadow-lg z-20">
         My Story
       </span>
-      <div className="grid grid-rows-3 grid-cols-2 gap-8 z-20 lg:grid-cols-1">
-        <p>
+      <div className="grid grid-rows-3 grid-cols-2 gap-8 z-20 lg:grid-rows-1 lg:grid-cols-1 md:grid-rows-1 sm:grid-rows-1">
+        <div>
           In highschool, I was that kid that seemed to never be paying
           attention, but at the end of the each semester I was the one with A+.
           <br /> <br />
@@ -22,8 +22,8 @@ const Story = () => {
           applications. I am going to learn Next.js as well. I have started my
           career with HTML, CSS and Javascript and now it ended up React.js and
           TailwindCSS. Short and sweet!
-        </p>
-        <p>
+        </div>
+        <div>
           As I mentioned before, I am geniunly in love with graphic design. So I
           decided to start learning UI/UX as well.
           <br /> <br />
@@ -31,17 +31,15 @@ const Story = () => {
           the industry. I like the people I meet. I like the coding. I like to
           make the web a better place. Hopefully we cross paths in the real
           world someday and we can make it a better place together.
-        </p>
-        <div className="flex flex-wrap justify-between items-center sm:flex-col">
-          <Link to="/about">
-            <div className="bg-blob5 bg-contain bg-center bg-no-repeat flex justify-center items-center m-0 m-auto text-sm w-28 p-12 text-gray-300 text-shadow-lg transform hover:scale-110 hover:text-gray-100 hover:transition duration-500 ease-in-out ">
-              Back
-            </div>
-          </Link>
-          <div className="flex justify-center m-0 m-auto items-center pb-16">
+          <div className="flex justify-center m-0 m-auto items-center mt-20">
             <Vase />
           </div>
         </div>
+        <Link to="/about">
+          <div className="bg-blob5 bg-contain bg-center bg-no-repeat flex justify-center items-center m-0 m-auto text-sm w-28 p-12 text-gray-300 text-shadow-lg transform hover:scale-110 hover:text-gray-100 hover:transition duration-500 ease-in-out">
+            Back
+          </div>
+        </Link>
       </div>
     </div>
   );
