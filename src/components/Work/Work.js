b7f9ddd135939936bd25f6314b0cc427ebed4bac
Fojan Babaali <fozhanmail@gmail.com>
2021-11-29 00:23:58 -0800

    Works Update

--- src/components/Work/Work.js
+++ src/components/Work/Work.js
@@ -5,7 +5,7 @@ const Work = (props) => {
   let { title, description, link } = props;
   return (
     <div className="flex items-center justify-center">
-      <div className="bg-light bg-opacity-30 w-full h-auto text-left p-4 rounded-xl shadow-md">
+      <div className="bg-light bg-opacity-30 w-full h-auto text-left p-4 rounded-xl shadow-md transform hover:scale-105 hover:text-gray-100 hover:transition duration-500 ease-in-out">
         <header className="bg-blob6 bg-contain bg-center bg-no-repeat flex items-center text-center justify-center w-64 font-bold text-lg text-gray-300 p-8">
           {title}
         </header>
